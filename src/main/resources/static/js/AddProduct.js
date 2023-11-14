030dbcbe4f555764cc1e721fb6bc13b46737870c
Ramij <ramijnalpur@gmail.com>
2023-11-14 21:21:19 +0530

    Adding selectable options in Add Product

--- src/main/resources/static/js/AddProduct.js
+++ src/main/resources/static/js/AddProduct.js
@@ -55,3 +55,71 @@ async function addProduct() {
 }
 
 const apiEndpoint = 'http://localhost:4040/api/product-list';
+document.getElementById('designName').addEventListener('mouseenter', function() {
+            showAllOptions();
+        });
+// Function to suggest subcategories based on user input
+let options = []; // Array to store subcategory names
+
+fetch('http://localhost:4040/api/design/names', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+})
+.then(response => response.json())
+.then(data => {
+    options = data; // Store subcategory names in the array
+})
+.catch(error => {
+    console.error('API request error:', error);
+});
+
+  function filterOptions() {
+const inputText = document.getElementById('designName').value;
+
+     const regex = new RegExp(inputText, 'i');
+      const filteredOptions = options.filter(option => regex.test(option));
+
+      const optionsList = document.getElementById('options-list');
+      optionsList.innerHTML = '';
+
+      if (filteredOptions.length > 0) {
+        filteredOptions.forEach(option => {
+          const optionElement = document.createElement('div');
+          optionElement.textContent = option;
+          optionElement.addEventListener('click', () => {
+            document.getElementById('designName').value = option;
+            optionsList.style.display = 'none';
+          });
+          optionsList.appendChild(optionElement);
+        });
+        optionsList.style.display = 'block';
+      } else {
+        optionsList.style.display = 'none';
+      }
+    }
+
+     function showAllOptions() {
+        const optionsList = document.getElementById('options-list');
+        if (optionsList.style.display === 'none') {
+          optionsList.innerHTML = '';
+          options.forEach(option => {
+            const optionElement = document.createElement('div');
+            optionElement.textContent = option;
+            optionElement.addEventListener('click', () => {
+              document.getElementById('designName').value = option;
+              optionsList.style.display = 'none';
+            });
+            optionsList.appendChild(optionElement);
+          });
+          optionsList.style.display = 'block';
+        }
+      }
+
+      document.addEventListener('click', (event) => {
+        const optionsList = document.getElementById('options-list');
+        if (!event.target.matches('#subCategoryName') && !event.target.matches('#options-list div')) {
+          optionsList.style.display = 'none';
+        }
+      });
