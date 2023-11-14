431b8c8b09a0779a91ea39b25127ab680cf2cc80
Ramij <ramijnalpur@gmail.com>
2023-11-14 22:27:17 +0530

    Code Refactoring

--- src/main/resources/static/js/AddProduct.js
+++ src/main/resources/static/js/AddProduct.js
@@ -1,125 +1,106 @@
-async function addProduct() {
-    const designName = document.getElementById("designName").value;
-    const quantity = parseInt(document.getElementById("quantity").value);
-    const currentCost = parseFloat(document.getElementById("currentCost").value);
-    const sizeName = document.getElementById("sizeName").value;
-    const color = document.getElementById("color").value;
+const apiEndpoint = 'http://localhost:4040/api/product-list';
+const designNamesEndpoint = 'http://localhost:4040/api/design/names';
 
-    if (!designName || !quantity || !currentCost || !sizeName || !color) {
-        alert("All fields are required!");
-        return;
-    }
-
-    if (isNaN(quantity) || quantity < 1) {
-        alert("Quantity must be a positive integer!");
-        return;
-    }
-
-    if (isNaN(currentCost) || currentCost <= 0) {
-        alert("Cost must be a positive number!");
-        return;
-    }
-
-    const productData = {
-        quantity: quantity,
-        cost: currentCost,
-        designName: designName,
-        size: {
-            sizeName: sizeName,
-        },
-        color: color,
-    };
-
+// Function to fetch design names
+async function fetchDesignNames() {
     try {
-        const response = await fetch(apiEndpoint, {
-            method: 'POST',
+        const response = await fetch(designNamesEndpoint, {
+            method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(productData),
         });
 
-        if (response.status >= 200 && response.status<300) {
-            alert("Product created successfully!");
-            document.getElementById("quantity").value = "";
-            document.getElementById("currentCost").value = "";
-            document.getElementById("sizeName").value = "";
-            document.getElementById("color").value = "";
+        if (response.ok) {
+            const data = await response.json();
+            return data;
         } else {
-            alert("Failed to create the product. Please try again.");
+            throw new Error('Failed to fetch design names');
         }
     } catch (error) {
-        alert("An error occurred while sending the request.");
-        console.error(error);
+        console.error('Error fetching design names:', error);
+        return [];
     }
 }
 
-const apiEndpoint = 'http://localhost:4040/api/product-list';
-document.getElementById('designName').addEventListener('mouseenter', function() {
-            showAllOptions();
+// Function to show design name options
+function showAllOptions() {
+    const optionsList = document.getElementById('options-list');
+    if (optionsList.style.display === 'none') {
+        optionsList.innerHTML = '';
+        options.forEach(option => appendOptionElement(option, optionsList));
+        optionsList.style.display = 'block';
+    }
+}
+
+// Function to append an option element to the options list
+function appendOptionElement(option, optionsList) {
+    const optionElement = document.createElement('div');
+    optionElement.textContent = option;
+    optionElement.addEventListener('click', () => {
+        document.getElementById('designName').value = option;
+        optionsList.style.display = 'none';
+    });
+    optionsList.appendChild(optionElement);
+}
+
+// Function to filter options based on user input
+function filterOptions() {
+    const inputText = document.getElementById('designName').value;
+
+    const regex = new RegExp(inputText, 'i');
+    const filteredOptions = options.filter(option => regex.test(option));
+
+    const optionsList = document.getElementById('options-list');
+    optionsList.innerHTML = '';
+
+    if (filteredOptions.length > 0) {
+        filteredOptions.forEach(option => {
+            appendOptionElement(option, optionsList);
         });
+        optionsList.style.display = 'block';
+    } else {
+        optionsList.style.display = 'none';
+    }
+}
+
 // Function to suggest subcategories based on user input
 let options = []; // Array to store subcategory names
 
-fetch('http://localhost:4040/api/design/names', {
+fetch(designNamesEndpoint, {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json',
     }
 })
-.then(response => response.json())
-.then(data => {
-    options = data; // Store subcategory names in the array
-})
-.catch(error => {
-    console.error('API request error:', error);
+    .then(response => response.json())
+    .then(data => {
+        options = data; // Store subcategory names in the array
+    })
+    .catch(error => {
+        console.error('API request error:', error);
+    });
+document.getElementById('designName').addEventListener('focus', function () {
+    showAllOptions();
 });
 
-  function filterOptions() {
-const inputText = document.getElementById('designName').value;
 
-     const regex = new RegExp(inputText, 'i');
-      const filteredOptions = options.filter(option => regex.test(option));
-
-      const optionsList = document.getElementById('options-list');
-      optionsList.innerHTML = '';
-
-      if (filteredOptions.length > 0) {
-        filteredOptions.forEach(option => {
-          const optionElement = document.createElement('div');
-          optionElement.textContent = option;
-          optionElement.addEventListener('click', () => {
-            document.getElementById('designName').value = option;
-            optionsList.style.display = 'none';
-          });
-          optionsList.appendChild(optionElement);
-        });
-        optionsList.style.display = 'block';
-      } else {
+document.addEventListener('click', (event) => {
+    const optionsList = document.getElementById('options-list');
+    if (!event.target.matches('#subCategoryName') && !event.target.matches('#options-list div')) {
         optionsList.style.display = 'none';
-      }
     }
+});
 
-     function showAllOptions() {
-        const optionsList = document.getElementById('options-list');
-        if (optionsList.style.display === 'none') {
-          optionsList.innerHTML = '';
-          options.forEach(option => {
-            const optionElement = document.createElement('div');
-            optionElement.textContent = option;
-            optionElement.addEventListener('click', () => {
-              document.getElementById('designName').value = option;
-              optionsList.style.display = 'none';
-            });
-            optionsList.appendChild(optionElement);
-          });
-          optionsList.style.display = 'block';
-        }
-      }
 
-      document.addEventListener('click', (event) => {
-        const optionsList = document.getElementById('options-list');
-        if (!event.target.matches('#subCategoryName') && !event.target.matches('#options-list div')) {
-          optionsList.style.display = 'none';
-        }
-      });
+function getURLParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+}
+
+// Extract the designName parameter from the URL and set it as the value of the input field
+const designName = getURLParameter('designName');
+const designNameInput = document.getElementById('designName');
+if (designNameInput && designName) {
+    designNameInput.value = designName;
+}
--- src/main/resources/static/js/CreateDesign.js
+++ src/main/resources/static/js/CreateDesign.js
@@ -1,73 +1,66 @@
 function createDesign() {
-    const form = document.getElementById('design-form');
-    const formData = new FormData(form);
-    const subCategoryName = formData.get('subCategoryName');
-    formData.delete('subCategoryName');
-    const formDataObject = {};
-    for (const [key, value] of formData.entries()) {
-        formDataObject[key] = value;
-    }
+  const form = document.getElementById('design-form');
+  const formData = new FormData(form);
+  const subCategoryName = formData.get('subCategoryName');
+  formData.delete('subCategoryName');
+  const formDataObject = {};
+  for (const [key, value] of formData.entries()) {
+    formDataObject[key] = value;
+  }
 
-    // Get the image file separately
-    const imageInput = document.getElementById('image');
-    const imageFile = imageInput.files[0];
+  // Get the image file separately
+  const imageInput = document.getElementById('image');
+  const imageFile = imageInput.files[0];
 
-    // API request with subCategoryName as a query parameter
-    const apiUrl = `http://localhost:4040/api/design?subCategoryName=${subCategoryName}`;
+  // API request with subCategoryName as a query parameter
+  const apiUrl = `http://localhost:4040/api/design?subCategoryName=${subCategoryName}`;
 
-    // Create a new FormData object and append the image
-    const combinedData = new FormData();
-    combinedData.append('image', imageFile);
+  // Create a new FormData object and append the image
+  const combinedData = new FormData();
+  combinedData.append('image', imageFile);
+  combinedData.append('data', new Blob([JSON.stringify(formDataObject)], { type: 'application/json' }));
 
-    // Add the data as a JSON string with a specified content type
-//    const dataObject = {
-//        designName: formData.get(''),
-//        description: 'YourDescription',
-//        creatorName: 'YourCreatorName'
-//    };
-    combinedData.append('data', new Blob([JSON.stringify(formDataObject)], { type: 'application/json' }));
+  // Append the remaining form data to combinedData
+  for (var pair of formData.entries()) {
+    combinedData.append(pair[0], pair[1]);
+  }
 
-    // Append the remaining form data to combinedData
-    for (var pair of formData.entries()) {
-        combinedData.append(pair[0], pair[1]);
-    }
-
-    fetch(apiUrl, {
-        method: 'POST',
-        body: combinedData
-    })
+  fetch(apiUrl, {
+    method: 'POST',
+    body: combinedData
+  })
     .then(response => {
-        if (response.ok) {
-            // Handle success
-            alert('Design created successfully!');
-        } else {
-            // Handle error
-            alert('Failed to create design');
-        }
+      if (response.ok) {
+        // Handle success
+        alert('Design created successfully!');
+      } else {
+        // Handle error
+        alert('Failed to create design');
+      }
     })
     .catch(error => {
-        console.error('API request error:', error);
+      console.error('API request error:', error);
     });
 }
 
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    const imageInput = document.getElementById("image");
-    const selectedImage = document.getElementById("selectedImage");
+  const imageInput = document.getElementById("image");
+  const selectedImage = document.getElementById("selectedImage");
 
-    imageInput.addEventListener("change", function () {
-        const selectedFile = imageInput.files[0];
+  imageInput.addEventListener("change", function () {
+    const selectedFile = imageInput.files[0];
 
-        if (selectedFile) {
-            const objectURL = URL.createObjectURL(selectedFile);
-            selectedImage.src = objectURL;
-        } else {
-            selectedImage.src = ""; // Clear the image if no file is selected
-        }
-    });
+    if (selectedFile) {
+      const objectURL = URL.createObjectURL(selectedFile);
+      selectedImage.src = objectURL;
+    } else {
+      selectedImage.src = ""; // Clear the image if no file is selected
+    }
+  });
 
-    // Your existing code here...
+  // Your existing code here...
 });
 
 
@@ -77,64 +70,64 @@ const subCategoryInput = document.getElementById('subCategoryName');
 
 // Fetch the list of subcategory names from the API
 fetch('http://localhost:4040/api/subcategory/names', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    }
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  }
 })
-.then(response => response.json())
-.then(data => {
+  .then(response => response.json())
+  .then(data => {
     options = data; // Store subcategory names in the array
-})
-.catch(error => {
+  })
+  .catch(error => {
     console.error('API request error:', error);
-});
+  });
 
-  function filterOptions() {
-const inputText = document.getElementById('subCategoryName').value;
+function filterOptions() {
+  const inputText = document.getElementById('subCategoryName').value;
 
-     const regex = new RegExp(inputText, 'i');
-      const filteredOptions = options.filter(option => regex.test(option));
+  const regex = new RegExp(inputText, 'i');
+  const filteredOptions = options.filter(option => regex.test(option));
 
-      const optionsList = document.getElementById('options-list');
-      optionsList.innerHTML = '';
+  const optionsList = document.getElementById('options-list');
+  optionsList.innerHTML = '';
 
-      if (filteredOptions.length > 0) {
-        filteredOptions.forEach(option => {
-          const optionElement = document.createElement('div');
-          optionElement.textContent = option;
-          optionElement.addEventListener('click', () => {
-            document.getElementById('subCategoryName').value = option;
-            optionsList.style.display = 'none';
-          });
-          optionsList.appendChild(optionElement);
-        });
-        optionsList.style.display = 'block';
-      } else {
+  if (filteredOptions.length > 0) {
+    filteredOptions.forEach(option => {
+      const optionElement = document.createElement('div');
+      optionElement.textContent = option;
+      optionElement.addEventListener('click', () => {
+        document.getElementById('subCategoryName').value = option;
         optionsList.style.display = 'none';
-      }
-    }
-    
-     function showAllOptions() {
-        const optionsList = document.getElementById('options-list');
-        if (optionsList.style.display === 'none') {
-          optionsList.innerHTML = '';
-          options.forEach(option => {
-            const optionElement = document.createElement('div');
-            optionElement.textContent = option;
-            optionElement.addEventListener('click', () => {
-              document.getElementById('subCategoryName').value = option;
-              optionsList.style.display = 'none';
-            });
-            optionsList.appendChild(optionElement);
-          });
-          optionsList.style.display = 'block';
-        }
-      }
-    
-      document.addEventListener('click', (event) => {
-        const optionsList = document.getElementById('options-list');
-        if (!event.target.matches('#subCategoryName') && !event.target.matches('#options-list div')) {
-          optionsList.style.display = 'none';
-        }
       });
+      optionsList.appendChild(optionElement);
+    });
+    optionsList.style.display = 'block';
+  } else {
+    optionsList.style.display = 'none';
+  }
+}
+
+function showAllOptions() {
+  const optionsList = document.getElementById('options-list');
+  if (optionsList.style.display === 'none') {
+    optionsList.innerHTML = '';
+    options.forEach(option => {
+      const optionElement = document.createElement('div');
+      optionElement.textContent = option;
+      optionElement.addEventListener('click', () => {
+        document.getElementById('subCategoryName').value = option;
+        optionsList.style.display = 'none';
+      });
+      optionsList.appendChild(optionElement);
+    });
+    optionsList.style.display = 'block';
+  }
+}
+
+document.addEventListener('click', (event) => {
+  const optionsList = document.getElementById('options-list');
+  if (!event.target.matches('#subCategoryName') && !event.target.matches('#options-list div')) {
+    optionsList.style.display = 'none';
+  }
+});
